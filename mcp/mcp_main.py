bdc2a9cde902e6169035e97c287a6c4170d100c1
zcy946 <zcy946@outlook.com>
2025-05-18 17:07:34 +0800

    基本代码完成

--- mcp/mcp_main.py
+++ mcp/mcp_main.py
@@ -1,19 +1,40 @@
 from mcp.server.fastmcp import FastMCP
 import tools
+import os
+from loguru import logger
 
 mcp = FastMCP("host info mcp")
-mcp.add_tool(tools.get_host_info)
-mcp.add_tool(tools.read_file)
-mcp.add_tool(tools.list_files)
-mcp.add_tool(tools.rename_file)
+
 
 # 同文件内可以通过注释直接添加
 # @mcp.tool()
 # def foo():
 #     return ""
 
+def initTools():
+    """初始化工具函数"""
+    mcp.add_tool(tools.read_file)
+    mcp.add_tool(tools.list_files)
+    mcp.add_tool(tools.rename_file)
+
+
+def initLogger():
+    """初始化日志"""
+    output_dir = "output"
+    os.makedirs(output_dir, exist_ok=True)
+    logger.add(os.path.join(output_dir, "output.log"), enqueue=True, rotation="10 MB", retention=5)
+
+
+def init():
+    """初始化"""
+    initLogger()
+    initTools()
+
+
 def main():
-    mcp.run("stdio") # sse
+    init()
+    logger.info("Start MCP server...")
+    mcp.run("stdio")
 
 
 if __name__ == "__main__":
--- mcp/tools.py
+++ mcp/tools.py
@@ -1,50 +1,16 @@
-import platform
-from typing import NoReturn, Union
-
-import psutil
-import subprocess
-import json
 from pathlib import Path
 import os
-
-base_dir = Path("./test")
-
-
-def get_host_info() -> str:
-    """get host information
-    Returns:
-        str: the host information in JSON string
-    """
-    info: dict[str, str] = {
-        "system": platform.system(),
-        "release": platform.release(),
-        "machine": platform.machine(),
-        "processor": platform.processor(),
-        "memory_gb": str(round(psutil.virtual_memory().total / (1024 ** 3), 2)),
-    }
-
-    cpu_count = psutil.cpu_count(logical=True)
-    if cpu_count is None:
-        info["cpu_count"] = "-1"
-    else:
-        info["cpu_count"] = str(cpu_count)
-
-    try:
-        cpu_model = subprocess.check_output(
-            ["sysctl", "-n", "machdep.cpu.brand_string"]
-        ).decode().strip()
-        info["cpu_model"] = cpu_model
-    except Exception:
-        info["cpu_model"] = "Unknown"
-
-    return json.dumps(info, indent=4)
+from loguru import logger
 
 
-def read_file(name: str) -> str:
+BASE_DIR = Path("E:/学习资料/作业/人工智能实训/PY/ShallowSeek/test")
+
+def read_file(file_path: str) -> str:
     """Return file content. If not exist, return error message.
     """
+    logger.info(f"call read_file({file_path})")
     try:
-        with open(base_dir / name, "r") as f:
+        with open(file_path, "r") as f:
             content = f.read()
         return content
     except Exception as e:
@@ -52,25 +18,28 @@ def read_file(name: str) -> str:
 
 
 def list_files() -> list[str]:
+    logger.info(f"call  list_files()")
     file_list = []
-    for item in base_dir.rglob("*"):
+    for item in BASE_DIR.rglob("*"):
         if item.is_file():
-            file_list.append(str(item.relative_to(base_dir)))
+            file_list.append(str(item.absolute()))
+    logger.info(f"   return {file_list}")
     return file_list
 
 
 def rename_file(name: str, new_name: str) -> str:
+    logger.info(f"call rename_file({name}, {new_name})")
     try:
-        new_path = base_dir / new_name
-        if not str(new_path).startswith(str(base_dir)):
-            return "Error: new_name is outside base_dir."
+        new_path = BASE_DIR / new_name
+        if not str(new_path).startswith(str(BASE_DIR)):
+            return "Error: new_name is outside BASE_DIR."
 
         os.makedirs(new_path.parent, exist_ok=True)
-        os.rename(base_dir / name, new_path)
+        os.rename(BASE_DIR / name, new_path)
         return f"File '{name}' successfully renamed to '{new_name}'."
     except Exception as e:
         return f"An error occurred: {e}"
 
 
 if __name__ == "__main__":
-    # print(get_absolute_path("../test/images/135358_1_UTC+8-20250517131254.png"))
+    list_files()
