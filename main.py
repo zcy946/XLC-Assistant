193545002ae04a601a8c0f5c1d04ab9412ef79e9
zcy946 <zcy946@outlook.com>
2025-05-18 19:09:11 +0800

    暂存

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
+import os
 import sys
-
+from loguru import logger
 from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.deepseek import DeepSeekProvider
 from pydantic_ai.mcp import MCPServerStdio
-
-import tools
+from aioconsole import ainput
 
 model = OpenAIModel(
     model_name="deepseek-chat",
@@ -20,7 +20,6 @@ server = MCPServerStdio(
 
 agent = Agent(model,
               system_prompt="你是一位经验丰富的程序员",
-              # tools=[tools.read_file, tools.list_files, tools.rename_file],
               mcp_servers=[server])
 
 
@@ -30,23 +29,39 @@ async def main():
         # 启动 MCP 服务器
         async with agent.run_mcp_servers():
             while True:
-                user_input = input("Input (type 'exit' to quit): ")
+                # 异步接收用户输入，防止阻塞
+                user_input = await ainput("(输入`exit`退出)>: ")
                 if user_input.lower() == 'exit':
                     break
                 try:
                     # 同步运行 Agent
                     resp = await agent.run(user_input, message_history=history)
                     history = list(resp.all_messages())
-                    print("Output:", resp.output)
+                    logger.success("AI: {}", resp.output)
+                except KeyboardInterrupt:
+                    logger.info("正在退出...")
                 except Exception as e:
-                    print(f"处理输入时出错：{e}", file=sys.stderr)
+                    logger.error(f"处理输入时出错：{e}", file=sys.stderr)
     except Exception as e:
-        print(f"Error starting MCP server or agent: {e}")
+        logger.critical(f"启动MCP服务器或Agent失败: {e}")
+
+
+def initLogger():
+    """初始化日志"""
+    output_dir = "output"
+    os.makedirs(output_dir, exist_ok=True)
+    logger.add(os.path.join(output_dir, "ShallowSeek_Client.log"), enqueue=True, rotation="10 MB", retention=5)
 
 
 if __name__ == "__main__":
+    initLogger()
     try:
         asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("程序已由用户终止")
     except RuntimeError as e:
-        print(f"事件循环错误：{e}", file=sys.stderr)
+        logger.critical(f"事件循环错误：{e}")
+        sys.exit(1)
+    except Exception as e:
+        logger.critical(f"未知错误：{e}")
         sys.exit(1)
--- mcp/mcp_main.py
+++ mcp/mcp_main.py
@@ -3,7 +3,7 @@ import tools
 import os
 from loguru import logger
 
-mcp = FastMCP("host info mcp")
+mcp = FastMCP("ShallowSeek MCP")
 
 
 # 同文件内可以通过注释直接添加
@@ -22,7 +22,7 @@ def initLogger():
     """初始化日志"""
     output_dir = "output"
     os.makedirs(output_dir, exist_ok=True)
-    logger.add(os.path.join(output_dir, "output.log"), enqueue=True, rotation="10 MB", retention=5)
+    logger.add(os.path.join(output_dir, "ShallowSeek_MCPServer.log"), enqueue=True, rotation="10 MB", retention=5)
 
 
 def init():
